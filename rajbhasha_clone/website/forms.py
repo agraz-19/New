a26d086a76557ffa147c64a4724c5f69c8408efe
Divya <divya9531reddy@gmail.com>
2026-01-21 10:36:36 +0530

    changes

--- rajbhasha_clone/website/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Employee
-
-class EmployeeForm(forms.ModelForm):
-    class Meta:
-        model = Employee
-        # Exclude the 'lastupdate' field since it's non-editable
-        fields = ['empcode', 'ename', 'hname', 'desig', 'gazet', 'prabodh', 'praveen', 'pragya', 'parangat', 'typing', 'hindiproficiency', 'superannuationDate']
