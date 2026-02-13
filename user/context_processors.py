d35008dcd5337c976f5ccf10855acff78bb6e961
agraz gopu <gagraz22@ifheindia.org>
2026-02-13 13:28:58 +0530

    deleted unrelated

--- user/context_processors.py
+++
@@ -1,7 +0,0 @@
-def global_settings(request):
-    if not hasattr(request, 'session'):
-        return {}
-    return {
-        'current_lang': request.session.get('lang', 'en'),
-        'role': request.session.get('active_role', 'user')
-    }
